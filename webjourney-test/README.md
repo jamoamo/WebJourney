d52d74004a8496d7b58fcd27d70018019ce5c60f
James Amoore <amoore.james@gmail.com>
2024-07-04 12:38:25 +0300

    Include a Test Module
    Update POM with parent
    
    Rename test module
    
    Reformat POM

---
+++ webjourney-test/README.md
@@ -0,0 +1,2 @@
+# webjourney-test
+ 
---
+++ webjourney-test/src/main/java/io/github/jamoamo/webjourney/test/TestCreator.java
@@ -0,0 +1,96 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2024 James Amoore.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package io.github.jamoamo.webjourney.test;
+
+import io.github.jamoamo.webjourney.api.web.AElement;
+import io.github.jamoamo.webjourney.api.web.IBrowser;
+import io.github.jamoamo.webjourney.reserved.entity.EntityCreator;
+import io.github.jamoamo.webjourney.reserved.entity.EntityDefn;
+import io.github.jamoamo.webjourney.reserved.entity.XEntityDefinitionException;
+import io.github.jamoamo.webjourney.reserved.entity.XEntityFieldScrapeException;
+import java.util.ArrayList;
+
+/**
+ * Entity Creation tester.
+ * @author James Amoore
+ * @param <T> the type of the entity to be created
+ */
+public final class TestCreator<T>
+{
+	private final Class<T> entityClass;
+
+	/**
+	 * Constructor.
+	 * @param entityClass The entity class to test creation of.
+	 */
+	public TestCreator(Class<T> entityClass)
+	{
+		this.entityClass = entityClass;
+	}
+
+	/**
+	 * Create the entity using the provided browser.
+	 * @param browser the browser.
+	 * @return the CreationResult.
+	 */
+	public CreationResult<T> createEntityWith(IBrowser browser)
+	{
+		CreationResult result = new CreationResult();
+		try
+		{
+			EntityCreator<T> creator = new EntityCreator<>(new EntityDefn<>(this.entityClass), false, new ArrayList<>());
+			result.setCreatedEntiy(creator.createNewEntity(browser));
+			result.setResult(CreationResult.Result.SUCCESS);
+		}
+		catch(XEntityFieldScrapeException | XEntityDefinitionException ex)
+		{
+			result.setResult(CreationResult.Result.FAILED);
+			result.setFailureException(ex);
+		}
+		return result;
+	}
+	
+	/**
+	 * Create the entity for the provided element and browser.
+	 * @param element the element
+	 * @param browser the browser
+	 * @return the creation result
+	 */
+	public CreationResult<T> createEntityWith(AElement element, IBrowser browser)
+	{
+		CreationResult result = new CreationResult();
+		try
+		{
+			EntityCreator<T> creator = new EntityCreator<>(new EntityDefn<>(this.entityClass), element, new ArrayList<>());
+			result.setCreatedEntiy(creator.createNewEntity(browser));
+			result.setResult(CreationResult.Result.SUCCESS);
+		}
+		catch(XEntityFieldScrapeException | XEntityDefinitionException ex)
+		{
+			result.setResult(CreationResult.Result.FAILED);
+			result.setFailureException(ex);
+		}
+		return result;
+	}
+}
